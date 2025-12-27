9dcc85750db8f84dc658ea4209ad6b9c1e977b9d
KimmSiddique <kimmsiddique777@gmail.com>
2025-12-27 13:24:44 +0600

    added utility_functions.rs -> Functions added: thread_sleep_for_ms() take_int_input() take_string_input() Added unit tests

---
+++ src/utility/utility_functions.rs
@@ -0,0 +1,62 @@
+use std::io::{self, Write};
+use std::thread;
+use std::time::Duration;
+
+pub fn thread_sleep_for_ms(duration: u64) {
+    thread::sleep(Duration::from_millis(duration));
+}
+
+pub fn take_int_input(message: &str) -> i32 {
+    loop {
+        let mut user_input = String::new();
+
+        print!("{message}: ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        match io::stdin().read_line(&mut user_input) {
+            Ok(_) => match user_input.trim().parse::<i32>() {
+                Ok(input) => return input,
+                Err(error) => {
+                    println!("Invalid integer value: {error}");
+                }
+            },
+            Err(error) => {
+                println!("Error reading input: {error}");
+            }
+        }
+    }
+}
+
+pub fn take_string_input(message: &str) -> String {
+    loop {
+        let mut user_input = String::new();
+
+        print!("{message}: ");
+        io::stdout().flush().expect("Failed to flush stdout");
+
+        match io::stdin().read_line(&mut user_input) {
+            Ok(_) => return user_input,
+            Err(error) => println!("Error reading input: {error}"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_take_int_input() {
+        let number = take_int_input("Enter a number");
+        println!("The number you typed was: {number}");
+        assert!(true);
+    }
+
+    #[test]
+    fn test_take_string_input() {
+        let string = take_string_input("Enter a word");
+        println!("You entered: {string}");
+        assert!(true);
+    }
+}
+
